d18d00c2a7277bf9a77baccf4166c53d0c56de6c
Utkarsh <utkarsh.yadav5@gmail.com>
2018-11-16 15:34:24 +0530

    AlbumDetail and Card added

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
+    state = { albums: [] };
+    componentWillMount() {
+        axios.get('http://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({ albums: response.data }));
+    }
+
+    renderAlbums() {
+        return this.state.albums.map(album => 
+            <AlbumDetail key={album.title} album={album} />
+        );
+    }
+
     render() {
         return (
             <View>
-                <Text>Album List</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
