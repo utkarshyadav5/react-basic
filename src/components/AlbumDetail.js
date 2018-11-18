877ed1bef9a6c6782f4b826afa4789c38fa13809
Utkarsh <utkarsh.yadav5@gmail.com>
2018-11-19 02:45:34 +0530

    Buttons added

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
-    const { title, artist, thumbnail_image, image } = album;
+    const { title, artist, thumbnail_image, image, url } = album;
     const { headerContentStyle, headerTextStyle, thumbnailStyle, thumbnailContainerStyle, imageStyle } = styles;
     return (
         <Card>
@@ -28,9 +28,11 @@ const AlbumDetail = ({ album }) => {
                     source={{ uri: image }} 
                 />
             </CardSection>
-            
+
             <CardSection>
-                <Button />
+                <Button onPress={() => Linking.openURL(url)}>
+                    Buy Now
+                </Button>
             </CardSection>
         </Card>
     );
