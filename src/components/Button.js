3ef8b0d0e65634d0186b3c1fdc92ae231c8edc01
Utkarsh <utkarsh.yadav5@gmail.com>
2018-11-19 01:09:02 +0530

    Card Section completed with scroll fixed

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+const Button = () => {
+    return (
+        <Text>Click Me!!</Text>
+    );
+};
+
+export default Button;
